3dd4c75ce9aeef97f701d94cfacfe2256659f843
IgorAugust0 <79866605+IgorAugust0@users.noreply.github.com>
2021-09-01 16:46:43 -0300

    Delete Roteiro 3 directory

--- Roteiro 3/ex16.pl
+++
@@ -1,7 +0,0 @@
-% 16. A linguagem formal aPar eh muito simples: ela consiste em todas as
-% strings contendo um numero par de as e nada mais. Note que a string
-% vazia pertence a aPar. Escreva uma DCG que gere aPar.
-
-s --> [].
-s --> [a],s,[a].
-
