bdf0d6b26eb6dc727073302dee93edcd7e5c1438
IgorAugust0 <79866605+IgorAugust0@users.noreply.github.com>
2021-09-01 16:40:29 -0300

    Delete Roteiro 3 directory

--- Roteiro 3/ex24.pl
+++
@@ -1,5 +0,0 @@
-% 24. Crie um predicado narranjos(M,P,N) que calcule o nuumero total N
-% de arranjos simples possiveis em uma lista com M elementos, tomados
-% P a P. Use este predicado para calcular o numero de diferentes
-% arranjos para os dois exercicios anteriores e verifique se todos eles
-% foram gerados.
--- Roteiro 3/ex7.pl
+++
@@ -1,9 +0,0 @@
-% 7. Chamaremos uma lista de duplicada se ela eh formada de dois blocos
-% consecutivos de elementos que sao exatamente os mesmos. Por exemplo,
-% [a,b,c,a,b,c] eh duplicada, pois ela eh formada de [a,b,c] seguida por
-% [a,b,c]. Tambem eh duplicada a lista[fu,ba,fu,ba]. Por outro lado, a
-% lista [fu,ba,fu] n˜a eh duplicada. Escreva um predicado
-% duplicada(Lista) que eh verdadeiro quando Lista eh uma lista
-% duplicada.
-
-duplicada(Lista) :- append(X, X, Lista).
